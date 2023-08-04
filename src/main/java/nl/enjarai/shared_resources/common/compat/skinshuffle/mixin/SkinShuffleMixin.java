0c14bf5e3fca02b4323046ab7122799379f10c6a
enjarai <enjarai@protonmail.com>
2023-08-04 19:27:44 +0200

    Mineblock collab! He didn't participate.

---
+++ src/main/java/nl/enjarai/shared_resources/common/compat/skinshuffle/mixin/SkinShuffleMixin.java
@@ -0,0 +1,31 @@
+package nl.enjarai.shared_resources.common.compat.skinshuffle.mixin;
+
+import nl.enjarai.shared_resources.api.GameResourceHelper;
+import nl.enjarai.shared_resources.common.registry.GameResources;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.nio.file.Path;
+
+@Pseudo
+@Mixin(targets = "com.mineblock11.skinshuffle.SkinShuffle")
+public abstract class SkinShuffleMixin {
+    @Shadow(aliases = "DATA_DIR")
+    private @Mutable static Path DATA_DIR;
+
+    @Dynamic
+    @Inject(
+            remap = false,
+            method = "<clinit>",
+            at = @At("RETURN")
+    )
+    private static void sharedresources$modifySkinShuffleDataDir(CallbackInfo ci) {
+        Path newDir = GameResourceHelper.getPathFor(GameResources.SKINSHUFFLE_DATA);
+
+        if (newDir != null) {
+            DATA_DIR = newDir;
+        }
+    }
+}
--- src/main/java/nl/enjarai/shared_resources/common/registry/GameResources.java
+++ src/main/java/nl/enjarai/shared_resources/common/registry/GameResources.java
@@ -4,9 +4,11 @@ import net.fabricmc.loader.api.FabricLoader;
 import nl.enjarai.shared_resources.api.*;
 import nl.enjarai.shared_resources.common.SharedResources;
 
+import static nl.enjarai.shared_resources.common.SharedResources.id;
 import static nl.enjarai.shared_resources.versioned.Versioned.TEXT;
 
 public class GameResources implements SharedResourcesEntrypoint {
+    // Folders
     public static final ResourceDirectory RESOURCEPACKS = new ResourceDirectoryBuilder("resourcepacks")
             .setDisplayName(TEXT.translatable("shared_resources.directory.resourcepacks"))
             .build();
@@ -39,7 +41,7 @@ public class GameResources implements SharedResourcesEntrypoint {
             )
             .build();
 
-
+    // Files
     public static final ResourceFile OPTIONS = new ResourceFileBuilder("options.txt")
             .setDisplayName(TEXT.translatable("shared_resources.file.options"))
             .setDescription(TEXT.translatable("shared_resources.file.options.description"))
@@ -54,6 +56,7 @@ public class GameResources implements SharedResourcesEntrypoint {
             .setDescription(TEXT.translatable("shared_resources.file.hotbars.description"))
             .build();
 
+    // Compat
     public static final ResourceDirectory SHADERPACKS = new ResourceDirectoryBuilder("shaderpacks")
             .setDisplayName(TEXT.translatable("shared_resources.directory.shaderpacks"))
             .build();
@@ -71,23 +74,31 @@ public class GameResources implements SharedResourcesEntrypoint {
             .setDisplayName(TEXT.translatable("shared_resources.directory.replay_recordings"))
             .overridesDefaultDirectory()
             .build();
+    public static final ResourceDirectory SKINSHUFFLE_DATA = new ResourceDirectoryBuilder("config/skinshuffle")
+            .setDisplayName(TEXT.translatable("shared_resources.directory.skinshuffle_data"))
+            .requiresRestart()
+            .overridesDefaultDirectory()
+            .build();
 
     @Override
     public void registerResources(GameResourceRegistry registry) {
-        registry.register(SharedResources.id("resourcepacks"), RESOURCEPACKS);
-        registry.register(SharedResources.id("saves"), SAVES);
-        registry.register(SharedResources.id("config"), CONFIG);
-        registry.register(SharedResources.id("screenshots"), SCREENSHOTS);
-        registry.register(SharedResources.id("datapacks"), DATAPACKS);
+        // Directories
+        registry.register(id("resourcepacks"), RESOURCEPACKS);
+        registry.register(id("saves"), SAVES);
+        registry.register(id("config"), CONFIG);
+        registry.register(id("screenshots"), SCREENSHOTS);
+        registry.register(id("datapacks"), DATAPACKS);
 
-        registry.register(SharedResources.id("options"), OPTIONS);
-        registry.register(SharedResources.id("servers"), SERVERS);
-        registry.register(SharedResources.id("hotbars"), HOTBARS);
+        // Files
+        registry.register(id("options"), OPTIONS);
+        registry.register(id("servers"), SERVERS);
+        registry.register(id("hotbars"), HOTBARS);
 
         // Only compat if a mod is available.
-        if (checkLoaded("iris")) registry.register(SharedResources.id("shaderpacks"), SHADERPACKS);
-        if (checkLoaded("litematica")) registry.register(SharedResources.id("schematics"), SCHEMATICS);
-        if (checkLoaded("replaymod")) registry.register(SharedResources.id("replay_recordings"), REPLAY_RECORDINGS);
+        if (checkLoaded("iris")) registry.register(id("shaderpacks"), SHADERPACKS);
+        if (checkLoaded("litematica")) registry.register(id("schematics"), SCHEMATICS);
+        if (checkLoaded("replaymod")) registry.register(id("replay_recordings"), REPLAY_RECORDINGS);
+        if (checkLoaded("skinshuffle")) registry.register(id("skinshuffle_data"), SKINSHUFFLE_DATA);
     }
 
     private static boolean checkLoaded(String modid) {
